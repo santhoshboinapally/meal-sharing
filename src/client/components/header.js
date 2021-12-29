aeabbf9cf10c79c71b193b3463b44dad499cf3e0
Santhosh Boinapally <64426776+santhoshboinapally@users.noreply.github.com>
2021-12-29 20:21:50 +0100

    Update header.js

--- src/client/components/header.js
+++ src/client/components/header.js
@@ -1,22 +1,13 @@
 import React from "react";
 import { NavigationBar } from "./navigationbar";
-import mslogo from "../assets/images/mslogo.png";
 function Header() {
     return (
          <center>
         <div className="header">
-<<<<<<< HEAD
             <h1>Meal Sharing</h1>
-=======
-         <img  src ={mslogo}   alt="mylogo" width="150px" height="100px"  />
->>>>>>> 8623ea0afa7ba41403f39a3baf3069e0f1155e38
          <NavigationBar />
         </div>
         </center>  
     );
 }
-<<<<<<< HEAD
 export default Header; 
-=======
-export default Header; 
->>>>>>> 8623ea0afa7ba41403f39a3baf3069e0f1155e38
