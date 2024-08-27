6ce80ec35f5c81d5cb9b512404949eb419233d25
Santhosh Boinapally <64426776+santhoshboinapally@users.noreply.github.com>
2024-08-27 08:48:00 +0200

    Update server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-const port = parseInt(process.env.PORT, 10) || process.env.API_PORT;
+const port = parseInt(process.env.CLIENT_PORT, 10) || process.env.API_PORT;
 
 app.listen(port, () => {
   console.log(`Backend api available at ${process.env.API_PATH}`);
